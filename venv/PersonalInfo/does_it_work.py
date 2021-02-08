bd36f3e6c57c3737eefb2b304a525be7f84681e6
RhythmicGaming <Jsawdon@kennedytech.com>
2021-02-08 18:46:35 -0500

    added example uses of the api

--- venv/PersonalInfo/does_it_work.py
+++ venv/PersonalInfo/does_it_work.py
@@ -1,9 +1,24 @@
 #must be run once to verify login and create a personal app id
 #this will trigger 2 factor at least the first time, more if enabled on your account
 import robin_stocks
-from BasePersonalInfo.MyInfo import usr, pwd
+from PersonalInfo.MyInfo import usr, pwd
 
+#login with your creds
 login = robin_stocks.login(usr, pwd)
+#dictionary of owned shares
 my_stocks = robin_stocks.build_holdings()
 for key,value in my_stocks.items():
      print(key,value)
+#list of price info every 5m for a span='week'
+list_prices = robin_stocks.stocks.get_stock_historicals('LABP',interval='5minute')
+#setup for dictionary of change
+change_dict = {}
+counter = 0
+for price in list_prices:
+    counter += 1
+    print(counter, price['open_price'], price['close_price'])
+    change_dict.update({counter: (float(price['open_price']) - float(price['close_price'])) / float(price['open_price'])})
+#rate of change per hour of LABP in a dictionary
+print(change_dict)
+
+
