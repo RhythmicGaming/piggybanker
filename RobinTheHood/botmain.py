f5beac251dd9ccd013b1b4b6a4983251afe34821
Jaylond Sawdon <jsawdon@kennedytech.com>
2021-02-09 23:05:27 -0500

    added simple error handling and added to help command

--- RobinTheHood/botmain.py
+++ RobinTheHood/botmain.py
@@ -1,9 +1,8 @@
 import discord
 import os
 from discord.ext import commands
-from PersonalInfo.MyInfo import TOKEN
 import robin_stocks
-from PersonalInfo.MyInfo import usr, pwd
+from PersonalInfo.MyInfo import usr, pwd,TOKEN
 
 HELP_DICT = {'hello':'$hello bot will respond hello!',
              'hist':'$hist written $hist TICKER INTERVAL SPAN where interval can be 5minute, hour, day, month etc; span is the length of time in question, when used without a span default is week',
@@ -32,21 +31,36 @@ async def on_message(message):
 
     elif message.content.startswith('$hist'):
         split = message.content.split()
-        if not split[3]:
-            split[3] = 'week'
+        if len(split)<2:
+            await message.channel.send(f"please try $help hist")
+            return
+        elif len(split) < 4:
+            split.append('week')
+        elif len(split) !=3:
+            await message.channel.send(f"please try $help hist")
+            return
         list_prices = robin_stocks.stocks.get_stock_historicals(split[1], interval=split[2], span=split[3])
         for price in list_prices:
             await message.channel.send(f"{price}")
 
+
     elif message.content.startswith('$check'):
         split = message.content.split()
-        this_price = robin_stocks.stocks.get_latest_price(f'{split[1]}')
-        await message.channel.send(f'{this_price}')
+        if len(split)!=2:
+            await message.channel.send("please try $help check")
+            return
+        else:
+            this_price = robin_stocks.stocks.get_latest_price(f'{split[1]}')
+            await message.channel.send(f'{this_price}')
 
     elif message.content.startswith('$check-details'):
         split = message.content.split()
-        this_price = robin_stocks.stocks.get_latest_price(f'{split[1]}')
-        await message.channel.send(f'{split[1]} {this_price[0]}')
+        if len(split) != 2:
+            await message.channel.send('try $help check-details')
+            return
+        else:
+            this_price = robin_stocks.stocks.get_latest_price(f'{split[1]}')
+            await message.channel.send(f'{split[1]} {this_price[0]}')
 
     elif message.content.startswith('$headline'):
         split = message.content.split()
@@ -61,11 +75,12 @@ async def on_message(message):
 
     elif message.content.startswith('$help'):
         split = message.content.split()
-        print(split)
+        msg = ''
         if len(split) != 1:
             if 'list' in split:
                 for k in HELP_DICT.keys():
-                    await message.channel.send(f"{k}")
+                    msg += f'{k}' + chr(10)
+                await message.channel.send(f"{msg}")
             else:
                 if split[1] in HELP_DICT.keys():
                     await message.channel.send(f"{split[1]}: {HELP_DICT[f'{split[1]}']}")
